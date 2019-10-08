f6ec63b4a436f0a936d726f3b1d6d7217116380a
jd52ru <Daemon52$>
2019-10-08 23:55:41 +0300

    Homework

--- src/server.py
+++ src/server.py
@@ -31,31 +31,54 @@ class Handler(LineOnlyReceiver):
 
         if self.login is not None:
             message = f"<{self.login}>: {message}"
-
+            #save current message to history
+            self.save_history(message.encode())
             for user in self.factory.clients:
                 if user is not self:
                     user.sendLine(message.encode())
         else:
             if message.startswith("login:"):
                 login = message.replace("login:", "")
-
+                #loop for connected users
+                for user in self.factory.clients:
+                    if user.login == login:
+                        #duplicate login found
+                        self.sendLine(f"Login {login} already used, try another one!".encode())
+                        #disconnecting
+                        self.transport.loseConnection()
                 self.login = login
                 print(f"New user: {login}")
                 self.sendLine("Welcome!!!".encode())
+                #send history
+                self.send_history()
             else:
-                self.sendLine("Неверный логин".encode())
+                self.sendLine("Wrong login!".encode())
 
+    #save message to history
+    def save_history(self, encoded_message):
+        self.factory.history.append(encoded_message)
+        #if history more 10 messages - remove first with index 0
+        while len(self.factory.history) > 10:
+            del self.factory.history[0]
+
+    #send history to user
+    def send_history(self):
+        for encoded_message in self.factory.history:
+            self.sendLine(encoded_message)
 
 class Server(ServerFactory):
     protocol = Handler
     clients: list
+    #list for history
+    history: list
 
     def __init__(self):
         self.clients = []
+        # init history
+        self.history = []
 
     def startFactory(self):
         print("Server started...")
-
 
 reactor.listenTCP(
     7410, Server()
